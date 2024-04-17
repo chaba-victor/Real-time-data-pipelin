e9f48f00496e8112cd609453ec1a92ff5d59240f
chaba-victor <chabavictor7@gmail.com>
2024-04-17 13:08:51 -0400

    added the 3rd pipeline - store

---
+++ candle to feature store/src/feature_store_api/api.py
@@ -0,0 +1,91 @@
+import os
+from typing import Optional, Dict, Any
+import logging
+
+import hopsworks
+import hsfs
+import pandas as pd
+
+from .types import FeatureGroupConfig
+
+logger = logging.getLogger()
+
+class FeatureGroup:
+
+    def __init__(
+        self,
+        feature_group: hsfs.feature_group.FeatureGroup,
+    ):  
+        self._fg = feature_group
+        
+    def write(
+        self,
+        value: Dict[str, Any],
+        keys: Optional[list] = None,
+        online_only: Optional[bool] = True,
+    ):
+        # If `keys` is prvovided, only keep these in the value
+        _value = {k: value[k] for k in keys} if keys else value
+        
+        # Convert the value to a Pandas DataFrame
+        df = pd.DataFrame.from_records([_value])
+
+        # Write the DataFrame to the feature group
+        logger.info(f'Writing {df} to feature group {self._fg.name} version={self._fg.version}')
+        if online_only:
+            self._fg.insert(df, write_options={"start_offline_backfill": False})
+        else:
+            raise NotImplementedError("Offline writing not implemented yet")
+
+class FeatureStore:
+    
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        project_name: Optional[str] = os.environ["HOPSWORKS_PROJECT_NAME"],
+    ):
+        self._api_key = api_key
+        if self._api_key is None:
+            try:
+                self._api_key = os.environ["HOPSWORKS_API_KEY"]
+            except KeyError:
+                raise KeyError(
+                    "No HOPSWORKS_API_KEY provided. Please provide an API key or set the HOPSWORKS_API_KEY environment variable."
+                )
+
+        self._project_name = project_name
+        if self._project_name is None:
+            try:
+                self._project_name = os.environ["HOPSWORKS_PROJECT_NAME"]
+            except KeyError:
+                raise KeyError(
+                    "No HOPSWORKS_PROJECT_NAME provided. Please provide a project name or set the HOPSWORKS_PROJECT_NAME environment variable."
+                )
+
+        self._fs = self._get_feature_store()
+
+    def get_or_create_feature_group(self, feature_group_config: FeatureGroupConfig) -> FeatureGroup:
+        """
+        Creates a feature group in the feature store if it does not exist, otherwise returns the existing feature group.
+        """
+        logger.info(f"Creating feature group {feature_group_config}")
+        
+        fg = self._fs.get_or_create_feature_group(
+            name=feature_group_config.name,
+            version=feature_group_config.version,
+            description=feature_group_config.description,
+            primary_key=feature_group_config.primary_key,
+            event_time=feature_group_config.event_time,
+            online_enabled=feature_group_config.online_enabled
+        )
+
+        logger.info(f"Created feature group {fg}")
+
+        return FeatureGroup(fg)
+    
+    def _get_feature_store(self) -> hsfs.feature_store.FeatureStore:
+        project = hopsworks.login(
+            project=self._project_name,
+            api_key_value=self._api_key,
+        )
+        return project.get_feature_store()
---
+++ candle to feature store/src/main.py
@@ -0,0 +1,57 @@
+import os
+import logging
+from typing import Optional
+
+# import redis
+
+from src.utils import initialize_logger, load_env_vars
+from src.feature_store_api.api import FeatureStore
+from src.feature_store_api.types import FeatureGroupConfig
+from src.app_factory import get_app
+
+logger = logging.getLogger()
+load_env_vars()
+
+KAFKA_INPUT_TOPIC = os.environ["input"]
+USE_LOCAL_KAFKA = True if os.environ.get('use_local_kafka') is not None else False
+OHLC_FEATURE_GROUP = FeatureGroupConfig(
+    name='ohlc_feature_group',
+    version=3,
+    description='OHLC data for crypto products',
+    primary_key=['timestamp', 'product_id'],
+    event_time='timestamp',
+    online_enabled=True,
+)
+
+def run():
+    """
+    Reads OHLC data from a Kafka topic and pushes the data to the Feature Store.
+    """
+    # Define Quix your application and settings
+    app = get_app(
+        consumer_group='json_ohlc_consumer_group',
+        use_local_kafka=USE_LOCAL_KAFKA,    
+    )
+    
+    # Define an input topic with this deserializer
+    input_topic = app.topic(KAFKA_INPUT_TOPIC, value_deserializer="json")
+
+    # Define some feature store access object
+    feature_store = FeatureStore()
+
+    # Create a feature group in the Feature Store
+    feature_group = feature_store.get_or_create_feature_group(OHLC_FEATURE_GROUP)
+
+    # Create a StreamingDataFrame and push incoming messages to the FeatureStore using a custom function
+    keys = ['timestamp', 'product_id', 'open', 'high', 'low', 'close']
+    sdf = app.dataframe(topic=input_topic).update(
+        lambda value: feature_group.write(value, keys=keys)
+    )
+
+    app.run(sdf)
+
+
+
+if __name__ == "__main__":
+    initialize_logger(config_path="logging.yaml")
+    run()
